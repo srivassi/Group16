fcb265f30222ace23e018aa68107a391bc42be1c
Alex <116267528+AlexMGarbalyauskas@users.noreply.github.com>
2025-02-23 01:33:51 +0000

    Add files via upload

---
+++ frontend/app/components/Footer/Footer.jsx
@@ -0,0 +1,24 @@
+
+//Imports
+import React from 'react';
+import './Footer.css';
+
+
+
+const Footer = () => {
+  return (
+    <div> 
+
+
+    <div className='footer-container'>
+      <div className='footerbox'>
+        <p>© 2025 Project 16 </p>
+      </div>
+    </div>
+
+
+    </div> 
+  );
+};
+
+export default Footer;
